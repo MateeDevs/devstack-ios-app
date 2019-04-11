f6ba00441050fcc08c67e5c3122a0a2539d787cb
Petr Chmelar <info@pchmelar.cz>
2019-04-11 14:33:34 +0200

    add UIFont+Extensions

---
+++ DevStack/UI/Extensions/UIFont+Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  UIFont+Extensions.swift
+//  DevStack
+//
+//  Created by Petr Chmelar on 11/04/2019.
+//  Copyright © 2019 Qest. All rights reserved.
+//
+
+import UIKit
+
+public extension UIFont {
+    
+    ///
+    /// Helper for easy access to custom font.
+    ///
+    /// - parameter ofSize: Font size to be used.
+    /// - returns: Custom font of a defined size.
+    ///
+    public static func customFont(ofSize size: CGFloat) -> UIFont? {
+        return UIFont(name: "CustomFont", size: size)
+    }
+    
+    ///
+    /// Helper for font size scaling.
+    ///
+    /// - parameter baseSize: Font size defined for 320x568 screen size.
+    /// - returns: Font size scaled for real screen size.
+    ///
+    public static func scalableFontSize(_ baseSize: CGFloat) -> CGFloat {
+        return ((UIScreen.main.bounds.size.height * baseSize / 568.0) * (320.0 / UIScreen.main.bounds.size.width))
+    }
+    
+}
