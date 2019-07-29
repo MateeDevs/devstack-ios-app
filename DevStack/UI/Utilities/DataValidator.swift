480689790748447ed1ad8e8a263583e5ff2daa7a
Petr Chmelar <filletzz@gmail.com>
2019-07-29 17:14:54 +0200

    Fix DataValidator

--- DevStack/UI/Utilities/DataValidator.swift
+++ DevStack/UI/Utilities/DataValidator.swift
@@ -13,11 +13,10 @@ public struct DataValidator {
     ///
     /// Checks whether a given email is valid
     ///
-    /// - parameter value: Email to be checked
+    /// - parameter email: Email to be checked
     /// - returns: True or false
     ///
-    public static func validateEmail(_ value: String?) -> Bool {
-        guard let email = value else { return false }
+    public static func validateEmail(_ email: String) -> Bool {
         do {
             let regex = try NSRegularExpression(pattern: "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$", options: [NSRegularExpression.Options.caseInsensitive])
             return regex.firstMatch(in: email, options: [], range: NSMakeRange(0, email.count)) != nil
@@ -34,19 +33,21 @@ public struct DataValidator {
     /// - parameter lowercase: Specify whether at least one lowercase letter is required
     /// - parameter uppercase: Specify whether at least one uppercase letter is required
     /// - parameter number: Specify whether at least one number is required
+    /// - parameter special: Specify whether at least one special character is required
     /// - returns: Error message, nil in case of valid password
     ///
     public static func validatePassword(_ password: String, length: Int = 8,
-                                        lowercase: Bool = false, uppercase: Bool = false, number: Bool = false) -> String? {
+                                        lowercase: Bool = false, uppercase: Bool = false,
+                                        number: Bool = false, special: Bool = false) -> String? {
         if password.count < 8 {
             return L10n.password_validation_length
-        } else if password.range(of: #"[a-z]"#, options: .regularExpression) == nil {
+        } else if lowercase, password.range(of: #"[a-z]"#, options: .regularExpression) == nil {
             return L10n.password_validation_lowercase
-        } else if password.range(of: #"[A-Z]"#, options: .regularExpression) == nil {
+        } else if uppercase, password.range(of: #"[A-Z]"#, options: .regularExpression) == nil {
             return L10n.password_validation_uppercase
-        } else if password.range(of: #"[0-9]"#, options: .regularExpression) == nil {
+        } else if number, password.range(of: #"[0-9]"#, options: .regularExpression) == nil {
             return L10n.password_validation_number
-        } else if password.range(of: #"[!&^%$#@()/]"#, options: .regularExpression) == nil {
+        } else if special, password.range(of: #"[!&^%$#@()/]"#, options: .regularExpression) == nil {
             return L10n.password_validation_symbol
         } else {
             return nil
