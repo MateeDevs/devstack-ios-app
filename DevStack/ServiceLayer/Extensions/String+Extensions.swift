9069c5aa96b2e7bc18b0ff8a96e4269005efd805
Petr Chmelar <petr.chmelar@matee.cz>
2020-02-21 18:22:18 +0100

    Formatter reusing

--- DevStack/ServiceLayer/Extensions/String+Extensions.swift
+++ DevStack/ServiceLayer/Extensions/String+Extensions.swift
@@ -13,14 +13,4 @@ extension String {
     var utf8Encoded: Data {
         data(using: .utf8)!
     }
-    
-    func convertFromISO8601(dateFormat: String = NetworkingConstants.iso8601DefaultFormat, timeZone: TimeZone? = TimeZone.current) -> Date {
-        let formatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .iso8601)
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.timeZone = timeZone
-        formatter.dateFormat = dateFormat
-        guard let date = formatter.date(from: self) else { return Date() }
-        return date
-    }
 }
