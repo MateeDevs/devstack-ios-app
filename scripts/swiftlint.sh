456b6466b279b1dba9c82fc9108e18b088c1d6f5
Petr Chmelar <petr.chmelar@matee.cz>
2020-03-29 21:21:35 +0200

    Xcode 11.4, improve swiftlint

--- scripts/swiftlint.sh
+++ scripts/swiftlint.sh
@@ -1,7 +1,7 @@
 SWIFT_LINT="${PODS_ROOT}/SwiftLint/swiftlint"
 SWIFT_LINT_CONFIG="${PODS_ROOT}/../.swiftlint.yml"
 
-if which swiftlint >/dev/null; then
+if which ${SWIFT_LINT} >/dev/null; then
     ${SWIFT_LINT} --config ${SWIFT_LINT_CONFIG} && ${SWIFT_LINT} autocorrect
 else
     echo "warning: SwiftLint not installed, 'pod install' or download from https://github.com/realm/SwiftLint"
