108f832bd44d0140adbd95f5c4865313f46a60cf
Petr Chmelar <petr.chmelar@matee.cz>
2020-03-31 23:56:38 +0200

    Run autocorrect before lint

--- scripts/swiftlint.sh
+++ scripts/swiftlint.sh
@@ -2,7 +2,7 @@ SWIFT_LINT="${PODS_ROOT}/SwiftLint/swiftlint"
 SWIFT_LINT_CONFIG="${PODS_ROOT}/../.swiftlint.yml"
 
 if which ${SWIFT_LINT} >/dev/null; then
-    ${SWIFT_LINT} --config ${SWIFT_LINT_CONFIG} && ${SWIFT_LINT} autocorrect
+    ${SWIFT_LINT} autocorrect && ${SWIFT_LINT} --config ${SWIFT_LINT_CONFIG}
 else
     echo "warning: SwiftLint not installed, 'pod install' or download from https://github.com/realm/SwiftLint"
 fi
