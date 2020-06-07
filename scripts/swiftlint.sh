84e438587208dcb834b4853e57cfdbdde9f4a8e5
Petr Chmelar <petr.chmelar@matee.cz>
2020-06-07 18:29:44 +0200

    WIP: Refactor swiftlint

--- scripts/swiftlint.sh
+++ scripts/swiftlint.sh
@@ -4,5 +4,6 @@ SWIFT_LINT_CONFIG="${PODS_ROOT}/../.swiftlint.yml"
 if which ${SWIFT_LINT} >/dev/null; then
     ${SWIFT_LINT} autocorrect && ${SWIFT_LINT} --config ${SWIFT_LINT_CONFIG}
 else
-    echo "warning: SwiftLint not installed, 'pod install' or download from https://github.com/realm/SwiftLint"
+    echo "error: SwiftLint not installed, 'pod install' or download from https://github.com/realm/SwiftLint"
+    exit 1
 fi
