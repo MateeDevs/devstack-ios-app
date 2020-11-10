c78038ce4651bab0bba92117003cdc6fed5b6ec2
Petr Chmelar <petr.chmelar@matee.cz>
2020-11-10 23:34:50 +0100

    Add reactive extensions for ImagePickerViewController

--- DevStack/UI/ViewControllers/ImagePickerViewController.swift
+++ DevStack/UI/ViewControllers/ImagePickerViewController.swift
@@ -8,55 +8,53 @@
 
 import UIKit
 
-public protocol ImagePickerControllerDelegate: class {
-    func photoSelected(image: UIImage?, sender: UIButton?)
+@objc public protocol ImagePickerViewControllerDelegate: class {
+    @objc optional func photoSelected(image: UIImage?)
 }
 
-open class ImagePickerViewController: BaseViewController {
-    
+open class ImagePickerViewController: InputViewController {
+
     // MARK: Stored properties
-	public var imagePickerTitle: String = L10n.image_picker_title
+    public var imagePickerTitle: String = L10n.image_picker_title
     public var imagePickerSubtitle: String = L10n.image_picker_subtitle
-    private var sender: UIButton?
-    
-    public weak var imagePickerDelegate: ImagePickerControllerDelegate?
-    
+
+    public weak var delegate: ImagePickerViewControllerDelegate?
+
     // MARK: Lifecycle methods
     override open func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     // MARK: Additional methods
     // swiftlint:disable:next private_action
     @IBAction public func addPicture(_ sender: UIButton) {
-        self.sender = sender
         view.endEditing(true)
-        
+
         // Setup action sheet with camera/library options
         let actionSheetController = UIAlertController(title: imagePickerTitle, message: imagePickerSubtitle, preferredStyle: .actionSheet)
-        
-		let photoLibrary = UIAlertAction(title: L10n.image_picker_library, style: .default, handler: { [weak self] _ in
+
+        let photoLibrary = UIAlertAction(title: L10n.image_picker_library, style: .default, handler: { [weak self] _ in
             self?.selectPhoto(sourceType: .photoLibrary)
         })
         actionSheetController.addAction(photoLibrary)
-        
-		let takePhotoByCamera = UIAlertAction(title: L10n.image_picker_camera, style: .default, handler: { [weak self] _ in
+
+        let takePhotoByCamera = UIAlertAction(title: L10n.image_picker_camera, style: .default, handler: { [weak self] _ in
             self?.selectPhoto(sourceType: .camera)
         })
         actionSheetController.addAction(takePhotoByCamera)
-        
-		let cancel = UIAlertAction(title: L10n.image_picker_cancel, style: .cancel, handler: nil)
+
+        let cancel = UIAlertAction(title: L10n.image_picker_cancel, style: .cancel, handler: nil)
         actionSheetController.addAction(cancel)
-        
+
         // Required for iPad
         actionSheetController.popoverPresentationController?.sourceView = view
         let sourceRect = CGRect(x: view.bounds.midX, y: view.bounds.midY, width: 0, height: 0)
         actionSheetController.popoverPresentationController?.sourceRect = sourceRect
         actionSheetController.popoverPresentationController?.permittedArrowDirections = []
-        
+
         present(actionSheetController, animated: true, completion: nil)
     }
-    
+
     private func selectPhoto(sourceType: UIImagePickerController.SourceType) {
         guard UIImagePickerController.isSourceTypeAvailable(sourceType) else { return }
         let imagePicker = UIImagePickerController()
@@ -73,13 +71,13 @@ extension ImagePickerViewController: UIImagePickerControllerDelegate, UINavigati
         didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
     ) {
         guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
-        
+
         // Save image to photo library if taken by camera
         if picker.sourceType == .camera {
             UIImageWriteToSavedPhotosAlbum(selectedImage, nil, nil, nil)
         }
-        
-        imagePickerDelegate?.photoSelected(image: selectedImage, sender: sender)
+
+        delegate?.photoSelected?(image: selectedImage)
         dismiss(animated: true, completion: nil)
     }
 }
