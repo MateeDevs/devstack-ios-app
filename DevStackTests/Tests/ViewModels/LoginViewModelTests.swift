bb779679e9aab2a1d11e38e6ea69b008df90dbfb
Petr Chmelar <petr.chmelar@matee.cz>
2021-02-11 01:39:57 +0100

    Change TestInput and TestOutput back to Input and Output

--- DevStackTests/Tests/ViewModels/LoginViewModelTests.swift
+++ DevStackTests/Tests/ViewModels/LoginViewModelTests.swift
@@ -16,24 +16,24 @@ class LoginViewModelTests: BaseTestCase {
 
     // MARK: Mock
 
-    private struct TestInput {
+    private struct Input {
         var email: String = ""
         var password: String = ""
         var loginButtonTaps: [Void] = []
         var registerButtonTaps: [Void] = []
 
-        static let loginEmpty = TestInput(loginButtonTaps: [()])
-        static let loginNonEmpty = TestInput(email: "email", password: "pass", loginButtonTaps: [()])
-        static let register = TestInput(registerButtonTaps: [()])
+        static let loginEmpty = Input(loginButtonTaps: [()])
+        static let loginNonEmpty = Input(email: "email", password: "pass", loginButtonTaps: [()])
+        static let register = Input(registerButtonTaps: [()])
     }
     
-    private struct TestOutput {
+    private struct Output {
         let flow: TestableObserver<LoginViewControllerFlow>
         let alertAction: TestableObserver<AlertAction>
         let loginButtonEnabled: TestableObserver<Bool>
     }
 
-    @discardableResult private func mockViewModel(for input: TestInput, providers: ProviderDependency = .mock()) -> TestOutput {
+    @discardableResult private func mockViewModel(for input: Input, providers: ProviderDependency = .mock()) -> Output {
         let viewModel = LoginViewModel(dependencies: ServiceDependency(dependencies: providers))
         
         scheduler.createColdObservable([.next(0, input.email)])
@@ -48,7 +48,7 @@ class LoginViewModelTests: BaseTestCase {
         scheduler.createColdObservable(input.registerButtonTaps.map { .next(0, $0) })
             .bind(to: viewModel.input.registerButtonTaps).disposed(by: disposeBag)
         
-        return TestOutput(
+        return Output(
             flow: testableOutput(from: viewModel.output.flow),
             alertAction: testableOutput(from: viewModel.output.alertAction),
             loginButtonEnabled: testableOutput(from: viewModel.output.loginButtonEnabled)
@@ -58,13 +58,13 @@ class LoginViewModelTests: BaseTestCase {
     // MARK: Tests for Output.flow
 
     func testFlowOutputForLoginEmpty() {
-        let output = mockViewModel(for: TestInput.loginEmpty)
+        let output = mockViewModel(for: Input.loginEmpty)
         scheduler.start()
         XCTAssertEqual(output.flow.events, [])
     }
 
     func testFlowOutputForLoginCorrectPassword() {
-        let output = mockViewModel(for: TestInput.loginNonEmpty)
+        let output = mockViewModel(for: Input.loginNonEmpty)
         scheduler.start()
         XCTAssertEqual(output.flow.events, [
             .next(0, .dismiss)
@@ -73,13 +73,13 @@ class LoginViewModelTests: BaseTestCase {
 
     func testFlowOutputForLoginWrongPassword() {
         let providers: ProviderDependency = .mock(networkResponse: .unauthorized)
-        let output = mockViewModel(for: TestInput.loginNonEmpty, providers: providers)
+        let output = mockViewModel(for: Input.loginNonEmpty, providers: providers)
         scheduler.start()
         XCTAssertEqual(output.flow.events, [])
     }
 
     func testFlowOutputForRegister() {
-        let output = mockViewModel(for: TestInput.register)
+        let output = mockViewModel(for: Input.register)
         scheduler.start()
         XCTAssertEqual(output.flow.events, [
             .next(0, .showRegistration)
@@ -89,7 +89,7 @@ class LoginViewModelTests: BaseTestCase {
     // MARK: Tests for Output.alertAction
 
     func testWhisperActionOutputForLoginEmpty() {
-        let output = mockViewModel(for: TestInput.loginEmpty)
+        let output = mockViewModel(for: Input.loginEmpty)
         scheduler.start()
         XCTAssertEqual(output.alertAction.events, [
             .next(0, .showWhisper(Whisper(error: L10n.invalid_credentials)))
@@ -97,7 +97,7 @@ class LoginViewModelTests: BaseTestCase {
     }
     
     func testAlertActionOutputForLoginCorrectPassword() {
-        let output = mockViewModel(for: TestInput.loginNonEmpty)
+        let output = mockViewModel(for: Input.loginNonEmpty)
         scheduler.start()
         XCTAssertEqual(output.alertAction.events, [
             .next(0, .showWhisper(Whisper(L10n.signing_in))),
@@ -107,7 +107,7 @@ class LoginViewModelTests: BaseTestCase {
 
     func testAlertActionOutputForLoginWrongPassword() {
         let providers: ProviderDependency = .mock(networkResponse: .unauthorized)
-        let output = mockViewModel(for: TestInput.loginNonEmpty, providers: providers)
+        let output = mockViewModel(for: Input.loginNonEmpty, providers: providers)
         scheduler.start()
         XCTAssertEqual(output.alertAction.events, [
             .next(0, .showWhisper(Whisper(L10n.signing_in))),
@@ -116,7 +116,7 @@ class LoginViewModelTests: BaseTestCase {
     }
 
     func testAlertActionOutputForRegister() {
-        let output = mockViewModel(for: TestInput.register)
+        let output = mockViewModel(for: Input.register)
         scheduler.start()
         XCTAssertEqual(output.alertAction.events, [])
     }
@@ -124,7 +124,7 @@ class LoginViewModelTests: BaseTestCase {
     // MARK: Tests for Output.loginButtonEnabled
 
     func testLoginButtonEnabledOutput() {
-        let output = mockViewModel(for: TestInput.loginNonEmpty)
+        let output = mockViewModel(for: Input.loginNonEmpty)
         scheduler.start()
         XCTAssertEqual(output.loginButtonEnabled.events, [
             .next(0, true),
@@ -139,7 +139,7 @@ class LoginViewModelTests: BaseTestCase {
         let networkProvider = NetworkProviderMock()
         let providers: ProviderDependency = .mock(networkProvider: networkProvider)
         
-        mockViewModel(for: TestInput.loginNonEmpty, providers: providers)
+        mockViewModel(for: Input.loginNonEmpty, providers: providers)
         scheduler.start()
         
         XCTAssertEqual(networkProvider.firedRequests, 1)
