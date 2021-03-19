4b08db9f0e01770459f54b03860ac78fad7b53bb
Petr Chmelar <petr.chmelar@matee.cz>
2021-03-19 01:27:15 +0100

    Add calls count and return values to DatabaseProviderMock and NetworkProviderMock

--- Tests/Mocks/Providers/DatabaseProviderMock.swift
+++ Tests/Mocks/Providers/DatabaseProviderMock.swift
@@ -7,13 +7,29 @@ import RealmSwift
 import RxSwift
 
 class DatabaseProviderMock: DatabaseProvider {
+    
+    var observableObjectCallsCount = 0
+    var observableObjectReturnValue: Observable<Object> = .empty()
+    
+    var observableCollectionCallsCount = 0
+    var observableCollectionReturnValue: Observable<[Object]> = .empty()
+    
+    var saveObjectCallsCount = 0
+    var saveObjectReturnValue: Observable<Object> = .empty()
+    
+    var saveCollectionCallsCount = 0
+    var saveCollectionReturnValue: Observable<[Object]> = .empty()
+    
+    var deleteAllCallsCount = 0
 
     func observableObject<T>(
         _ type: T.Type,
         id: String,
         primaryKeyName: String
     ) -> Observable<T> where T: Object {
-        return .empty()
+        observableObjectCallsCount += 1
+        guard let observableObjectReturnValue = observableObjectReturnValue as? Observable<T> else { return .empty() }
+        return observableObjectReturnValue
     }
 
     func observableCollection<T>(
@@ -22,17 +38,24 @@ class DatabaseProviderMock: DatabaseProvider {
         sortBy: String?,
         ascending: Bool
     ) -> Observable<[T]> where T: Object {
-        return .empty()
+        observableCollectionCallsCount += 1
+        guard let observableCollectionReturnValue = observableCollectionReturnValue as? Observable<[T]> else { return .empty() }
+        return observableCollectionReturnValue
     }
     
     func save<T>(_ object: T, model: UpdateModel) -> Observable<T> where T: Object {
-        return .empty()
+        saveObjectCallsCount += 1
+        guard let saveObjectReturnValue = saveObjectReturnValue as? Observable<T> else { return .empty() }
+        return saveObjectReturnValue
     }
     
     func save<T>(_ objects: [T], model: UpdateModel) -> Observable<[T]> where T: Object {
-        return .empty()
+        saveCollectionCallsCount += 1
+        guard let saveCollectionReturnValue = saveCollectionReturnValue as? Observable<[T]> else { return .empty() }
+        return saveCollectionReturnValue
     }
 
     func deleteAll() {
+        deleteAllCallsCount += 1
     }
 }
