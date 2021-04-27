6ff2c146d2ea3a9a8f6679416e47b7b2b8ebb6d4
Petr Chmelar <petr.chmelar@matee.cz>
2021-04-27 18:29:01 +0200

    Add develop.yml to rename.sh

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -31,6 +31,7 @@ sed -i '' -e "s/${old_name_lowercase}/${new_name_lowercase}/g" ../scripts/twine.
 sed -i '' -e "s/${old_name}/${new_name}/g" ../scripts/swiftlint-analyze.sh
 sed -i '' -e "s/${old_name}/${new_name}/g" ../fastlane/Fastfile
 sed -i '' -e "s/${old_name_lowercase}/${new_name_lowercase}/g" ../fastlane/Fastfile
+sed -i '' -e "s/${old_name}/${new_name}/g" ../.github/workflows/develop.yml
 sed -i '' -e "s/${old_name}/${new_name}/g" ../scripts/setup.sh
 
 echo "✅ Renaming successful"
