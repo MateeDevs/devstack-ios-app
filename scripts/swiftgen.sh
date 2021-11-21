2e4ef01b1e2d92c8319b74a45ee2ca1d0acf6a16
Petr Chmelar <petr.chmelar@matee.cz>
2021-11-21 22:34:02 +0000

    Adjust twine, SwiftGen and Atlantis

--- scripts/swiftgen.sh
+++ scripts/swiftgen.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh -l
 
 echo "Generating identifiers for Colors and Images"
-mint run swiftgen run xcassets "PresentationLayer/Resources/Colors.xcassets" "PresentationLayer/Resources/Images.xcassets" -p "scripts/swiftgen-xcassets.stencil" --param publicAccess --output "PresentationLayer/Resources/Constants/Assets.swift"
+mint run swiftgen run xcassets "PresentationLayer/Sources/PresentationLayer/Resources/Colors.xcassets" "PresentationLayer/Sources/PresentationLayer/Resources/Images.xcassets" -p "scripts/swiftgen-xcassets.stencil" --param publicAccess --output "PresentationLayer/Sources/PresentationLayer/Resources/Constants/Assets.swift"
 
 echo "Generating identifiers for Localizable"
-mint run swiftgen run strings "PresentationLayer/Resources/Base.lproj/Localizable.strings" -p "scripts/swiftgen-strings.stencil" --param publicAccess --output "PresentationLayer/Resources/Constants/Localizable.swift"
+mint run swiftgen run strings "PresentationLayer/Sources/PresentationLayer/Resources/Base.lproj/Localizable.strings" -p "scripts/swiftgen-strings.stencil" --param publicAccess --output "PresentationLayer/Sources/PresentationLayer/Resources/Constants/Localizable.swift"
 
 echo "Generating identifiers for Storyboards"
-mint run swiftgen run ib "PresentationLayer" -p "scripts/swiftgen-ib.stencil" --output "PresentationLayer/Resources/Constants/Storyboards.swift"
+mint run swiftgen run ib "PresentationLayer" -p "scripts/swiftgen-ib.stencil" --output "PresentationLayer/Sources/PresentationLayer/Resources/Constants/Storyboards.swift"
--- scripts/twine.sh
+++ scripts/twine.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh -l
 
 echo "Generating Localizable files for specified languages"
-twine generate-localization-file $TWINE_FOLDER/devstack/strings.txt ./PresentationLayer/Resources/Base.lproj/Localizable.strings --lang en
-twine generate-localization-file $TWINE_FOLDER/devstack/strings.txt ./PresentationLayer/Resources/cs.lproj/Localizable.strings --lang cs
-twine generate-localization-file $TWINE_FOLDER/devstack/strings.txt ./PresentationLayer/Resources/sk.lproj/Localizable.strings --lang sk
-twine generate-localization-file $TWINE_FOLDER/devstack/strings.txt ./PresentationLayer/Resources/en.lproj/Localizable.strings --lang en
+twine generate-localization-file $TWINE_FOLDER/devstack/strings.txt PresentationLayer/Sources/PresentationLayer/Resources/Base.lproj/Localizable.strings --lang en
+twine generate-localization-file $TWINE_FOLDER/devstack/strings.txt PresentationLayer/Sources/PresentationLayer/Resources/cs.lproj/Localizable.strings --lang cs
+twine generate-localization-file $TWINE_FOLDER/devstack/strings.txt PresentationLayer/Sources/PresentationLayer/Resources/sk.lproj/Localizable.strings --lang sk
+twine generate-localization-file $TWINE_FOLDER/devstack/strings.txt PresentationLayer/Sources/PresentationLayer/Resources/en.lproj/Localizable.strings --lang en
